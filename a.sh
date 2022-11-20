6a32aef25a48c06be605d6b45f1a6551d5eb2bb8
soban <muhammad.soban@cheetay.pk>
2022-11-20 10:03:32 +0500

    add

---
+++ a.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Enter the path of your folder?
+read folder
+echo What should be zip file name?
+read name
+sudo zip -r ${name}.zip ${folder}
+sed -i "/filepath=/c\var filepath='../$name.zip'" ./gwork/app.js
+sed -i "/filename=/c\var filename='$name.zip'" ./gwork/app.js
+
+cd ./gwork && node app.js && cd ..
